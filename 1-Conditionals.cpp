9dc6473958224b74ea1d972fb9237f96c7f884a6
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-12-15 20:15:18 +0530

    first commit

---
+++ 1-Conditionals.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Enter a number :";
+    int n; 
+    cin>>n;
+    if (n%5 ==0){
+        cout<<"number is Even";
+    }
+
+    else
+    {
+        cout<<"number is odd";
+    }
+}
---
+++ 2-.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    // program to finding largest number among 3 given number...
+    cout << "Enter 1st number: ";
+    cin >> a;
+
+    cout << "Enter 2nd number: ";
+    cin >> b;
+
+    cout << "Enter 3rd number: ";
+    cin >> c;
+
+    if (a > b && a > c) {
+        cout << "a is the largest number";
+    } else if (b > a && b > c) {
+        cout << "b is the largest number";
+    } else if (c > a && c > b) {
+        cout << "c is the largest number";
+    } else {
+        cout << "There is a tie among the numbers.";
+    }
+
+    return 0;
+}
